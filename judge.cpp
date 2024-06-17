b3f4587444a9f55ac318710b4ac392ac3e4b4f01
James Fang <113867753+jamesfang87@users.noreply.github.com>
2024-06-17 00:59:32 -0500

    Update judge.cpp

--- judge.cpp
+++ judge.cpp
@@ -4,14 +4,61 @@
 #include <filesystem>
 #include <algorithm>
 #include <vector>
+#include <cmath>
+#include <sstream>
 
-// there are leading and trailing double quotation marks for src_path so g++ can still compile
-// if there are spaces within the path name
-std::string src_path = "\"path/to/src_code.cpp\"";
-std::string test_data_path = "path/to/test_data";
-double time_limit = 2.0; // 2 s = 2000 ms
+std::string src_path = "\"path/to/src\"";
+std::string test_data_path = "path/to/testdata";
+double time_limit = 2.0; // 2s = 2000ms
 double memory_limit = 256; // 256mb
 std::string compiler_flags = " -std=c++17 -O2 -lm -Wall "; // keep the leading and trailing spaces
+
+double get_execution_time(const std::string& line) {
+    // check time limit
+    // runtime comes after the first occurrence of ":"
+    std::string time = line.substr(line.find(':') + 1);
+    return std::stod(time);
+}
+
+double get_memory_used(const std::string& line) {
+    // check memory limit
+    // max resident contains memory usage it occurs after "avgdata"
+    std::string mem = line.substr(line.find("avgdata ") + 8);
+    return std::stod(mem) / 1000;
+}
+
+std::vector<std::string> extract_output(const std::string& file_path) {
+    /*
+     * extracts output of a file into a vector
+     * each element in the vector is a line in the file
+     */
+    std::ifstream file(file_path);
+    std::vector<std::string> output;
+
+    // get next line in file
+    std::string line;
+    while (std::getline(file, line)) {
+        // append to vector
+        output.push_back(line);
+    }
+    file.close();
+
+    return output;
+}
+
+std::vector<std::string> clean_output(const std::vector<std::string>& raw_output) {
+    /*
+     * removes all whitespace from the end of a vector representing output to a file
+     */
+    std::vector<std::string> ret;
+    for (const std::string& line : raw_output) {
+        if (line.find_first_not_of(" \t\n\v\f\r") != std::string::npos) {
+            ret.push_back(line);
+        }
+    }
+    return ret;
+}
+
 
 void check(int exit_code, int test_number) {
     /*
@@ -22,138 +69,81 @@ void check(int exit_code, int test_number) {
      * RE: runtime error
      * WA: wrong answer
      */
+    std::cout << "Verdict for test case " << test_number << ": ";
 
-    // check for exit code
+    /*
+     * exit code check:
+     */
     if (exit_code != 0) {
-        std::cout << "\x1b[31m" << "RE with exit code: " << exit_code << "\x1b[0m";
+        std::cout << "\t  \x1b[31m" << "RE with exit code: " << exit_code << "\x1b[0m" << std::endl;
         return;
     }
 
-    std::ifstream user_out("user_out.txt");
-    std::ifstream expected(test_data_path + "/" + std::to_string(test_number) + ".out");
+    /*
+     * time and memory limit checks:
+     */
 
-    std::vector<std::string> output;
-    std::string line;
-    while (std::getline(user_out, line)) {
-        // no way any output for a program will have "user" AND "system" AND "elapsed" right??? right?!?
-        // there is no way
-        // no possible way
-        bool probably_time = line.find("user") != std::string::npos &&
-                             line.find("system") != std::string::npos &&
-                             line.find("elapsed") != std::string::npos;
+    // get the first line from runtimeinfo.txt
+    // it contains information about execution time and memory usage
+    std::ifstream runtime_info("runtimeinfo.txt");
+    std::string line; getline(runtime_info, line);
+    runtime_info.close();
 
-        if (!probably_time) {
-            output.push_back(line);
-        } else {
-            break;
-        }
-    }
-
-    // now line contains information about time and memory usage:
-
-    // check for time limit
-    std::string time = line.substr(line.find(':') + 1);
-    double execution_time = std::stod(time);
+    // check time limit
+    double execution_time = get_execution_time(line);
     if (execution_time > time_limit) {
-        std::cout << "\x1b[31m" << "TLE\n" << "\x1b[0m";
+        std::cout << "\x1b[31m" << "TLE\n" << "\x1b[0m" << std::endl;
         return;
     }
-    // check for memory limit
-    std::string mem = line.substr(line.find("avgdata ") + 8);
-    double memory_used = std::stod(mem) / 1000;
-    if (memory_used > memory_limit) {
-        std::cout << "\x1b[31m" << "MLE\n" << "\x1b[0m";
+
+    // check mem limit
+    double memory_usage = get_memory_used(line);
+    if (memory_usage > memory_limit) {
+        std::cout << "\x1b[31m" << "MLE\n" << "\x1b[0m" << std::endl;
         return;
     }
+
+    /*
+     * WA and AC checks:
+     */
+    // extract expected output into a vector
+    std::string expected_out_filepath = test_data_path + "/" + std::to_string(test_number) + ".out";
+    std::vector<std::string> expected_out = extract_output(expected_out_filepath);
+
+    // extract submission output into a vector
+    std::vector<std::string> submission_out = extract_output("submission_out.txt");
 
     // check for wrong answer
-    std::vector<std::string> expected_out;
-    while (std::getline(expected, line)) {
-        expected_out.push_back(line);
-    }
-
-    if (expected_out != output) {
+    if (clean_output(expected_out) != clean_output(submission_out)) {
         std::cout << "\x1b[31m" << "WA" << "\x1b[0m";
     } else {
         std::cout << "\x1b[92m" << "AC" << "\x1b[0m";
     }
 
-    // output execution time and memory usage
-    std::cout << "   \t" << execution_time * 1000 << " ms " << memory_used << " mb";
+    // output execution time and memory usage only if WA or AC
+    std::cout << "  \t" << execution_time * 1000 << " ms " << round(memory_usage * 10) / 10 << " mb" << std::endl;
 }
 
 void run(int test_number) {
     // base cmd: "./{executable_name}", simply runs the program
-    std::string cmd = "TIMEFORMAT='%4R'; gtime ./submission";// + src_path;
+    std::string cmd = "gtime -o runtimeinfo.txt ./submission";
 
     // redirect input and output:
     std::string input_name = test_data_path + "/" + std::to_string(test_number) + ".in";
     cmd += " < " + input_name;
-    cmd += " > user_out.txt"; // program output, execution time, memory usage written here
+    cmd += " > submission_out.txt"; // program output, execution time, memory usage written here
 
-    // Open the output file for writing
-    std::ofstream user_out("user_out.txt");
-    if (!user_out.is_open()) {
-        std::cerr << "Error opening file: user_out.txt" << std::endl;
-        return;
-    }
-
-    // "2>&1" directs stderr to the same output location as stdout, either a file or the console
-    cmd += " 2>&1";
-
-    // Open a pipe for reading the time command's output
-    FILE* pipe = popen(cmd.c_str(), "r");
-    if (!pipe) {
-        std::cerr << "Error opening pipe" << std::endl;
-        user_out.close();
-        return;
-    }
-
-    // Read from the pipe (stdin) char by char and write to the file
-    char ch;
-    while ((ch = static_cast<char>(fgetc(pipe))) != EOF) {
-        std::cout << ch;
-        user_out << ch;
-
-        if (ferror(pipe)) {
-            std::cerr << "Error reading from pipe" << std::endl;
-            pclose(pipe);
-            return;
-        }
-    }
-
-    int exit_code = pclose(pipe); // close the pipe and get the return code from the child
-    user_out.close();  // Close the output file
-
-    std::cout << "Verdict for test case " << test_number << ": ";
-    check(exit_code, test_number);
-    std::cout << std::endl;
+    // run program
+    int submission_ret_code = system(cmd.c_str());
+    check(WEXITSTATUS(submission_ret_code), test_number);
 }
 
 int compile() {
     std::string compile_command = "g++" + compiler_flags + src_path + " -o submission";
-    FILE* pipe = popen(compile_command.c_str(), "r");
-    if (!pipe) {
-        std::cerr << "Error: Could not open pipe for compilation" << std::endl;
-        return 1;
-    }
-
-    // Read compiler output line by line and display it
-    char ch;
-    while ((ch = static_cast<char>(fgetc(pipe))) != EOF) {
-        std::cout << ch;
-
-        if (ferror(pipe)) {
-            std::cerr << "Error reading from pipe" << std::endl;
-            pclose(pipe);
-            return 1;
-        }
-    }
-
-    int exit_code = pclose(pipe);
+    int exit_code = system(compile_command.c_str());
 
     // Check compilation exit code
-    if (exit_code != 0) {
+    if (WEXITSTATUS(exit_code) != 0) {
         std::cerr << "Compilation failed\n" << std::endl;
         return 1;
     } else {
@@ -163,9 +153,10 @@ int compile() {
 }
 
 int main() {
-    int ret_code = compile();
-    if (ret_code != 0) {
-        return 1;
+    int compile_ret_code = compile();
+    // exit program if submission does not compile
+    if (compile_ret_code != 0) {
+        return 0;
     }
 
     // iterator to the directory that the test data is stored in
@@ -178,11 +169,15 @@ int main() {
             [](auto& entry) { return entry.is_regular_file(); }
     ) / 2;
 
+    // run against test cases
     std::cout << "Results are displayed below:" << std::endl;
     for (int i = 1; i <= num_test_cases; i++) {
         // run executable with test case i as input
         run(i);
     }
-    std::filesystem::remove("user_out.txt");
+
+    // remove output files
+    std::filesystem::remove("submission_out.txt");
     std::filesystem::remove("submission");
+    std::filesystem::remove("runtimeinfo.txt");
 }
